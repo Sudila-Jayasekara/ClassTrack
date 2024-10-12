203c98da0bea21c9629186a4bf8aad10a6ac4f34
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-12 15:36:02 +0530

    Changed Id Format

--- backend/src/main/java/com/classtrack/backend/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.classtrack.backend.repository;
-
-import com.classtrack.backend.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
